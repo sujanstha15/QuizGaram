b12e099b0cd63a0b197aaf051caa0b6562826568
Sujan Shrestha <raju40rr@gmail.com>
2025-09-01 23:47:44 -0500

    project started

---
+++ quizpadham/quizpadham/src/main/java/com/quizpadham/QuizPadhamApplication.java
@@ -0,0 +1,13 @@
+package com.quizpadham;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuizPadhamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuizPadhamApplication.class, args);
+	}
+
+}
